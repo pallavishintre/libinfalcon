32529f1f2e34f007e40c20f192b7af5babf26de3
pallavishintre <pshintre@asu.edu>
2019-11-06 17:23:16 -0700

    Revert "Revert "Modified Experiment""
    This reverts commit 0727a894cf5fcd76628bff65c3d5a06315328d1c.

---
+++ examples/pid_controller/pid_controller.h
@@ -0,0 +1,17 @@
+#ifndef PD_CONTROLLER_H
+#define PD_CONTROLLER_H
+
+#include "util.h"
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <time.h>
+#include <sys/time.h>
+#include <math.h>
+
+#include <zmq.hpp>
+#include <unistd.h>
+
+#endif // PD_CONTROLLER_H
